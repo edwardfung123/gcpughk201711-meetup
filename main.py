421eee1484a67f8922b46181792fc11e14d3e8e7
Edward FUNG <edwardfung123@gmail.com>
2017-11-29 00:30:40 +0800

    Add jinja2.6

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import webapp2
 
 
 class MainPage(webapp2.RequestHandler):
+    @webapp2.cached_property
+    def jinja2(self):
+        # Returns a Jinja2 renderer cached in the app registry.
+        from webapp2_extras import jinja2
+        return jinja2.get_jinja2(app=self.app)
+
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Stalker!!')
+        data = {
+            'name': 'Edward',
+            }
+        html = self.jinja2.render_template('index.html', **data)
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(html)
 
 
 app = webapp2.WSGIApplication([
